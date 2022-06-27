853542802370f4ed7d594f571c8ced4bc4445a42
이성현 <marvel97@naver.com>
2022-06-27 11:17:19 +0900

    MySQL BOARD ADD

--- src/main/java/com/example/springboot/MainController.java
+++ src/main/java/com/example/springboot/MainController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @RequestMapping("/sbb")
+    @RequestMapping("/coolmarvel")
     @ResponseBody
     public String index() {
-        return "안녕하세요 sbb에 오신것을 환영합니다.";
+        return "안녕하세요 springBoot에 오신것을 환영합니다.";
     }
 
     @RequestMapping("/")
